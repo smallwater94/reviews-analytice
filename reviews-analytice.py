a8b3158fb58f2beac703e44e33491e2ce750d5e0
York Wu <smallwater94@gmail.com>
2021-03-01 14:57:52 +0800

    Stage 2 ver1

--- reviews-analytice.py
+++ reviews-analytice.py
@@ -6,3 +6,12 @@ with open('reviews.txt', 'r') as f:
 		if len(data) % 100000 == 0:
 			print('進度',pg,'%')
 			pg += 10
+print('檔案取完畢 共',len(data),'筆資料')
+
+allnum = 0
+con = 0
+while con < 1000000:
+	allnum += len(data[con])
+	con += 1
+avg = allnum / 1000000
+print(avg)
